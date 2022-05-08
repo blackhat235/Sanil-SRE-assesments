4dbd688b998a99937a1069c8b678a9f2034e0233
sunny <blackhat235@gmail.com>
2022-05-09 01:20:50 +0530

    terraform script added

---
+++ Terraform-assesment-07-05-22/securitygroups.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "allow various ports as per requirement" {
+  vpc_id      = aws_vpc.myvpc.id
+  name        = "allow-ssh"
+  description = "security group that allows various ports and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-ssh"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-80"
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-http"
+  }
+  
+  ingress {
+    from_port   = 9092
+    to_port     = 9092
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-9092"
+  }
+}
+
