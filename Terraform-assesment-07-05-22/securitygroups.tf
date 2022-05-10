fd427ea772c5e154490f97bea8a0582d4b331774
sunny <blackhat235@gmail.com>
2022-05-10 14:15:56 +0530

    sgedit

--- Terraform-assesment-07-05-22/securitygroups.tf
+++ Terraform-assesment-07-05-22/securitygroups.tf
@@ -8,23 +8,29 @@ resource "aws_security_group" "allow-ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
+  } 
+  
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  ingress {
     from_port   = 9092
     to_port     = 22
     protocol    = "tcp"
